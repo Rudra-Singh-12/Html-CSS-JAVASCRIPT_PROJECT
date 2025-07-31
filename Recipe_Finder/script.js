88e95f3a700e7f5853596053c8837e0030c3d72a
Rudra-Singh-12 <rudrapratapsingh0412@gmail.com>
2025-07-31 09:14:36 +0530

    modify the comment in recipe finder

--- Recipe_Finder/script.js
+++ Recipe_Finder/script.js
@@ -8,6 +8,7 @@ const mealDetails = document.getElementById("meal-details");
 const mealDetailsContent = document.querySelector(".meal-details-content");
 const backBtn = document.getElementById("back-btn");
 
+// api url 
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 const SEARCH_URL = `${BASE_URL}search.php?s=`;
 const LOOKUP_URL = `${BASE_URL}lookup.php?i=`;
@@ -26,6 +27,7 @@ searchInput.addEventListener("keypress", (e) => {
 });
 
 async function searchMeals() {
+    // getting the search value from user through input without whitespaces
     const searchTerm = searchInput.value.trim();
 
     // handle the edge case
@@ -34,18 +36,22 @@ async function searchMeals() {
             errorContainer.classList.remove("hidden");
         return;
     }
+    // searching the meals in try block and error is handle by catch block
     try {
+        // display the name for the search 
         resultHeading.textContent = `Searching for "${searchTerm}" ...`;
+        // value of mealsContainer is none
         mealsContainer.innerHTML = "";
+        // adding the features of class hidden from css
         errorContainer.classList.add("hidden");
 
         // fetching the meals from API
         const response = await fetch(`${SEARCH_URL}${searchTerm}`);
-
+        // getting the data from api in json format
         const data = await response.json();
-
+        // displaying the data in console
         console.log("data is here:", data);
-
+        // handle edge case
         if (data.meals === null) {
             // no meals found case
             resultHeading.textContent = ``;
@@ -53,22 +59,23 @@ async function searchMeals() {
             errorContainer.textContent = `No recipes found for "${searchTerm}".Try another search term!`;
             errorContainer.classList.remove("hidden");
         } else {
+          // displaying the meal  text content in web
             resultHeading.textContent = `Search results for "${searchTerm}":`;
-
+            // calling the displayMeals function to display the results from api
             displayMeals(data.meals);
             searchTerm.value = "";
 
         }
 
-    } catch (error) {
+    } catch (error) { // handling the error in this block
         errorContainer.textContent = "Something went wrong. Please try again later."
         errorContainer.classList.remove("hidden");
     }
 }
-
+// methods to display the content in browser
 function displayMeals(meals) {
     mealsContainer.innerHTML = "";
-    // loop through meals and create a card for each meal
+    // loop through meals and create a card for each meal from api result
     meals.forEach((meal) => {
         mealsContainer.innerHTML += `
       <div class="meal" data-meal-id="${meal.idMeal}">
@@ -82,23 +89,30 @@ function displayMeals(meals) {
     });
 }
 
+// handling the click event on meal card-> onclick the description about the meals is generated through api
 async function handleMealClick(e){
+  // mealEl card 
     const mealEl=e.target.closest(".meal");
-
+    // getting the id of the meal card
     const mealId=mealEl.getAttribute("data-meal-id");
+    // edge case if mealId is null -> return
     if(!mealId) return;
 
-
+    // handling the click event in try block
     try {
+      // getting the response from api
         const response=await fetch(`${LOOKUP_URL}${mealId}`);
-
+        // data from api
         const data=await response.json();
+        // displaying the data on console
         console.log(data);
-
+        // data,meals and data.meals[0] are coming from api response 
         if(data.meals && data.meals[0]){
+          // data of click meal
             const meal=data.meals[0];
-
+            // array to store the ingredient from api
             const ingredients=[];
+            // display the ingredient 
             for(let i=1;i<20;i++){
                 if(meal[`strIngredient${i}`] && meal[`strIngredient${i}`].trim()!==""){
                     ingredients.push({
@@ -144,6 +158,7 @@ async function handleMealClick(e){
       mealDetails.classList.remove("hidden");
       mealDetails.scrollIntoView({ behavior: "smooth" });
     }
+    // handling the edge case
   } catch (error) {
     errorContainer.textContent = "Could not load recipe details. Please try again later.";
     errorContainer.classList.remove("hidden");
